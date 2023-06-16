3a348ea5cb52c39633693000db222eda69f29842
quannh287 <quannh2871@gmail.com>
2023-06-16 22:16:36 +0700

    Init Chat Project

---
+++ app/src/main/java/com/example/chat/activities/SignInActivity.kt
@@ -0,0 +1,30 @@
+package com.example.chat.activities
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.chat.R
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        hideSoftKeyboard()
+        return super.dispatchTouchEvent(ev)
+    }
+
+    private fun hideSoftKeyboard() {
+        val view = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+}
